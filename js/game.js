2d8039f2afbf11fbcb61f78928b422e198ef2112
Danny <93397389+Dcastro99@users.noreply.github.com>
2022-02-02 21:27:21 -0800

    created item constructor

---
+++ js/game.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+
+function Items(name,x,y) {
+  this.name = name;
+  this.x = x;
+  this.y = y;
+}
+
+let laptop = new Items('laptop');
+let keyboard = new Items('keyboard');
+let mouse = new Items('mouse');
+let flashlight = new Items('flashlight');
+let backback = new Items('backback');
+let textBooks = new Items('textbooks');
