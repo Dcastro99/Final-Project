1ed4db9a140795f288e3ee8843f353db583d587f
laptopadventure <bazelart13@gmail.com>
2022-02-09 23:33:47 -0800

    repositioning

--- js/game.js
+++ js/game.js
@@ -112,9 +112,9 @@ function Inventory(pojoItems) {
     //first time setup, creates all items with their default vals
     // this.items.push(new Items('logo', false, '/index.html', '30px', '5rem', 'genericClick', 'this is a hint for logo!'));
     this.items.push(new Items('laptop', false, '/index.html', '3px', '8rem', 'laptopClick', 'this is a hint for laptops!'));
-    this.items.push(new Items('keyboard', false, '/classroom.html', '60px', '9rem', 'genericClick', 'this is a hint for keyboard!'));
+    this.items.push(new Items('keyboard', false, '/classroom.html', '60px', '16rem', 'genericClick', 'this is a hint for keyboard!'));
     this.items.push(new Items('mouse', false, '/classroom.html', '100px', '5rem', 'genericClick', 'this is a hint for mouse!'));
-    this.items.push(new Items('flashlight', false, '/index.html', '666px', '10rem', 'flashlightClick', 'this is a hint for flashlight!'));
+    this.items.push(new Items('flashlight', false, '/index.html', '666px', '14rem', 'flashlightClick', 'this is a hint for flashlight!'));
     this.items.push(new Items('backpack', false, '/index.html', '333px', '5rem', 'genericClick', 'this is a hint for backpack!'));
     this.items.push(new Items('textbooks', false, '/classroom.html', '555px', '5rem', 'genericClick', 'this is a hint for textbooks!'));
     this.items.forEach(item => item.render());
